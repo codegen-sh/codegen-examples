5f2e077432858896f6397217143aee39cff7007a
vishalshenoy <34020235+vishalshenoy@users.noreply.github.com>
2025-02-08 01:48:05 +0000

    Automated pre-commit update

--- examples/delete_dead_code/run.py
+++ examples/delete_dead_code/run.py
@@ -2,6 +2,7 @@ import codegen
 from codegen import Codebase
 from codegen.sdk.enums import ProgrammingLanguage
 
+
 @codegen.function("delete-dead-code")
 def run(codebase: Codebase):
     removed_functions_count = 0
